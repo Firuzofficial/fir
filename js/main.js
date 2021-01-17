fd94248dccb05530e20b46b4b2507cfd9843beef
Firuzofficial <77243024+Firuzofficial@users.noreply.github.com>
2021-01-17 23:03:49 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// const name = 'Firus Ib';
+// const sername = 'nameoff'
+
+// function toUpper(text) {
+// 	const upperCased = text.toUpperCase();
+// 	console.log(upperCased);
+// }
+
+// toUpper(sername);
+
+const name = 'Firuz';
+const age = 16;
+
+//console.log(`Hello my name is ${name} and i'm ${age}`);
+
+if(age > 18) {
+	console.log('you go!');
+} else if (age < 15) {
+ console.log('What are you doing here');
+}
+else {
+	console.log("You don't go");
+}
